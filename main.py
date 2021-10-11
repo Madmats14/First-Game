d8e62e9c01b0a10cea9664ff872e21875fc491c1
Madmats14 <hd.cuevas@yahoo.com.ar>
2021-10-11 17:15:01 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.constants import QUIT
+
+# initialize game
+pygame.init()
+
+# create screen
+screen = pygame.display.set_mode((800.600))
+
+# Title and icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("battleship.png")
+pygame.display.set_icon(icon)
+
+# game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
